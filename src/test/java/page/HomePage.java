2b1bb33dbcd68117b54a3fea4e8405f792b665bb
AZR88 <azrielamaldany@gmail.com>
2024-11-14 22:26:06 +0700

    Atutomatic chek for title & price by id

--- src/test/java/page/HomePage.java
+++ src/test/java/page/HomePage.java
@@ -11,9 +11,19 @@ import java.time.Duration;
 public class HomePage {
 
     public static By productImage = By.cssSelector("img.card-img-top.img-fluid");
-    public static By productTitle = By.xpath("//h4[@class='card-title']/a");
     public static By productPrice = By.xpath("//div[@class='card-block']/h5");
     public static By productDescription = By.xpath("//p[@id='article']");
+
+    // Metode untuk mendapatkan locator dinamis berdasarkan ID produk
+    public static By getProductTitleLocatorById(String productId, String expectedTitle) {
+        return By.xpath("//a[@href='prod.html?idp_=" + productId + "' and text()='" + expectedTitle + "']");
+    }
+    public static By getProductPriceLocator(String expectedPrice) {
+        return By.xpath("//h5[text()='" + expectedPrice + "']");
+    }
+
+
+
 
     // Metode untuk mendapatkan ukuran (size) elemen
     public static Dimension getElementSize(WebDriver driver, By locator) {
@@ -23,12 +33,28 @@ public class HomePage {
     }
 
     // Metode untuk membandingkan teks dari elemen title
-    public static boolean isTitleTextEqual(WebDriver driver, String expectedText) {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-        WebElement titleElement = wait.until(ExpectedConditions.visibilityOfElementLocated(productTitle));
-        String actualText = titleElement.getText();
-        return actualText.equals(expectedText);
+    public static boolean isTitleAndPriceTextEqual(WebDriver driver, String productId, String expectedTitle, String expectedPrice) {
+        // Dapatkan locator untuk judul produk
+        By productTitleLocator = getProductTitleLocatorById(productId, expectedTitle);
+        // Dapatkan locator untuk harga produk
+        By productPriceLocator = getProductPriceLocator(expectedPrice);
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+
+        WebElement titleElement = wait.until(ExpectedConditions.visibilityOfElementLocated(productTitleLocator));
+        String actualTitle = titleElement.getText();
+
+
+        WebElement priceElement = wait.until(ExpectedConditions.visibilityOfElementLocated(productPriceLocator));
+        String actualPrice = priceElement.getText();
+
+
+        return actualTitle.equals(expectedTitle) && actualPrice.equals(expectedPrice);
     }
+
+
+
 
     public static boolean isPriceTextEqual(WebDriver driver, String expectedText) {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
@@ -44,13 +70,7 @@ public class HomePage {
         return actualText.equals(expectedText);
     }
 
-
     public static Dimension getProductImageSize(WebDriver driver) {
         return getElementSize(driver, productImage);
-    }
-
-
-    public static Dimension getProductTitleSize(WebDriver driver) {
-        return getElementSize(driver, productTitle);
     }
 }
