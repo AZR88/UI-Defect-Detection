5a604dc8badaee53a4a5d19657f178eb8ec5ef8b
AZR88 <azrielamaldany@gmail.com>
2024-11-14 22:28:19 +0700

    Atutomatic chek for title & price by id

--- src/test/java/page/HomePage.java
+++ src/test/java/page/HomePage.java
@@ -14,7 +14,6 @@ public class HomePage {
     public static By productPrice = By.xpath("//div[@class='card-block']/h5");
     public static By productDescription = By.xpath("//p[@id='article']");
 
-    // Metode untuk mendapatkan locator dinamis berdasarkan ID produk
     public static By getProductTitleLocatorById(String productId, String expectedTitle) {
         return By.xpath("//a[@href='prod.html?idp_=" + productId + "' and text()='" + expectedTitle + "']");
     }
@@ -22,26 +21,19 @@ public class HomePage {
         return By.xpath("//h5[text()='" + expectedPrice + "']");
     }
 
-
-
-
-    // Metode untuk mendapatkan ukuran (size) elemen
     public static Dimension getElementSize(WebDriver driver, By locator) {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
         return element.getSize();
     }
 
-    // Metode untuk membandingkan teks dari elemen title
+
     public static boolean isTitleAndPriceTextEqual(WebDriver driver, String productId, String expectedTitle, String expectedPrice) {
-        // Dapatkan locator untuk judul produk
+
         By productTitleLocator = getProductTitleLocatorById(productId, expectedTitle);
-        // Dapatkan locator untuk harga produk
         By productPriceLocator = getProductPriceLocator(expectedPrice);
 
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-
-
         WebElement titleElement = wait.until(ExpectedConditions.visibilityOfElementLocated(productTitleLocator));
         String actualTitle = titleElement.getText();
 
