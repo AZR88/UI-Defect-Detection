c88b0afd2f12e9ee96238c4dcb7fabdfa39d7dd8
AZR88 <azrielamaldany@gmail.com>
2024-11-19 22:19:45 +0700

    update stepdef : price detect page 2 fix

--- src/test/java/stepDef/HomePage.java
+++ src/test/java/stepDef/HomePage.java
@@ -2,16 +2,20 @@ package stepDef;
 
 import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
 import java.util.Map;
 
 import static Helper.WebHelper.driver;
 import static org.junit.Assert.assertTrue;
+import static page.HomePage.getProductTitleLocatorById;
 
 public class HomePage {
-
 
 
     @When("check multiple titles and ids")
@@ -24,7 +28,7 @@ public class HomePage {
             String expectedPrice = row.get("price");
 
             try {
-                Thread.sleep(5000); // 5 detik delay
+                Thread.sleep(3000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -42,11 +46,41 @@ public class HomePage {
     }
 
 
-
-
-
     @Given("user is on homepage")
     public void userIsOnHomepage() {
         driver.get("https://www.demoblaze.com/");
+    }
+
+
+    @Then("click next to show next product where id = {string} and title = {string}")
+    public void clickNextToShowNextProductWhereTitleAndPrice(String arg0, String arg1) throws InterruptedException {
+        page.HomePage.ClickNext(driver, arg0, arg1);
+    }
+
+    @Then("check multiple titles and ids on next page")
+    public void checkMultipleTitlesAndIdsOnNextPage(DataTable dataTable) {
+        List<Map<String, String>> data = dataTable.asMaps(String.class, String.class);
+
+        for (Map<String, String> row : data) {
+            String productId = row.get("id");
+            String expectedTitle = row.get("title");
+            String expectedPrice = row.get("price");
+
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            boolean isTitleAndPriceCorrect = page.HomePage.isTitleAndPriceTextEqual(driver, productId, expectedTitle, expectedPrice);
+
+
+            System.out.println("ID Produk: " + productId);
+            System.out.println("Expected Title: " + expectedTitle);
+            System.out.println("Expected Price: " + expectedPrice);
+            System.out.println("Actual Title and Price: " + (isTitleAndPriceCorrect ? "Sesuai" : "Tidak Sesuai"));
+
+            assertTrue("Title atau Price tidak sesuai untuk produk dengan ID " + productId, isTitleAndPriceCorrect);
+        }
     }
 }
