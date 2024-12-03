21ad1050e56ee814cad4017704788dab08699006
AZR88 <azrielamaldany@gmail.com>
2024-12-03 07:59:51 +0700

    update helper

--- src/test/java/Helper/WebHelper.java
+++ src/test/java/Helper/WebHelper.java
@@ -3,6 +3,10 @@ package Helper;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 import java.time.Duration;
 
@@ -10,21 +14,55 @@ public class WebHelper {
 
     public static WebDriver driver;
 
-    public static void startDriver(){
-        ChromeOptions options = new ChromeOptions();
-        //options.addArguments("--headless");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--remote-allow-origins=*");
-        options.addArguments("--window-size=1920,1080");
-        driver = new ChromeDriver(options);
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
-        driver.get("https://www.demoblaze.com/");
+    public static void startDriver(String browser) {
+        switch (browser.toLowerCase()) {
+            case "chrome":
+                ChromeOptions chromeOptions = new ChromeOptions();
+                // Headless mode for Chrome
+                // chromeOptions.addArguments("--headless");
+                chromeOptions.addArguments("--no-sandbox");
+                chromeOptions.addArguments("--disable-dev-shm-usage");
+                chromeOptions.addArguments("--remote-allow-origins=*");
+                chromeOptions.addArguments("--window-size=1920,1080");
+                chromeOptions.addArguments("--start-maximized");
+                chromeOptions.addArguments("--disable-infobars");
+                chromeOptions.addArguments("--disable-extensions");
+                driver = new ChromeDriver(chromeOptions);
+                break;
 
+            case "firefox":
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                // Headless mode for Firefox
+                // firefoxOptions.addArguments("--headless");
+                firefoxOptions.addArguments("--no-sandbox");
+                firefoxOptions.addArguments("--disable-dev-shm-usage");
+                firefoxOptions.addArguments("--remote-allow-origins=*");
+                firefoxOptions.addArguments("--window-size=1920,1080");
+                driver = new FirefoxDriver(firefoxOptions);
+                break;
+
+            case "edge":
+                EdgeOptions edgeOptions = new EdgeOptions();
+                // Headless mode for Edge
+                // edgeOptions.addArguments("--headless");
+                edgeOptions.addArguments("--no-sandbox");
+                edgeOptions.addArguments("--disable-dev-shm-usage");
+                edgeOptions.addArguments("--remote-allow-origins=*");
+                edgeOptions.addArguments("--window-size=1920,1080");
+                driver = new EdgeDriver(edgeOptions);
+                break;
+
+            default:
+                throw new IllegalArgumentException("Unsupported browser: " + browser);
+        }
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.get("https://www.demoblaze.com/");
     }
 
-    public static void tearDown(){
-        driver.quit();
+    public static void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
