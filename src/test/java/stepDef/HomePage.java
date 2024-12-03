cdfca86a4fd40788029e1409935742daaf8addb0
AZR88 <azrielamaldany@gmail.com>
2024-12-03 19:55:51 +0700

    update stepdef : add navbar check function

--- src/test/java/stepDef/HomePage.java
+++ src/test/java/stepDef/HomePage.java
@@ -13,8 +13,7 @@ import java.util.Map;
 
 import static Helper.WebHelper.driver;
 import static org.junit.Assert.assertTrue;
-import static page.HomePage.CheckHomebutton;
-import static page.HomePage.getProductTitleLocatorById;
+import static page.HomePage.*;
 
 public class HomePage {
 
@@ -103,5 +102,32 @@ public class HomePage {
     @When("check Navbar {string} button")
     public void checkNavbarButton(String arg0) {
         CheckHomebutton(driver,arg0);
+    }
+
+    @Then("Check {string} button")
+    public void checkButton(String arg1) {
+        CheckAboutbutton(driver,arg1);
+
+    }
+
+    @Then("Check the {string} button")
+    public void checkTheButton(String arg0) {
+        CheckCartbutton(driver,arg0);
+    }
+
+
+    @Then("Check a {string} button")
+    public void checkAButton(String arg0) {
+        CheckLoginbutton(driver,arg0);
+    }
+
+    @Then("check {string} buton")
+    public void checkButon(String arg0) {
+        CheckSignupbutton(driver,arg0);
+    }
+
+    @Then("Chek {string} button")
+    public void chekButton(String arg0) {
+        Checkcontactbutton(driver,arg0);
     }
 }
